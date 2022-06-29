3ed071a749045b4eabf0a2e1cc60ecdf0f922d75
gayatripatil22 <gayatrip185@gamil.com>
2022-06-29 10:30:39 +0530

    add helpers and models of role,user and ability

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  belongs_to :role
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, 
          :confirmable, :trackable
+  def admin?
+    role.role == 'admin'
+  end
 end
